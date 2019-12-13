0d5c46299e75a5ee7c0a252889141371f78b79cf
Georgios Leandros Kyriazis <leankyr@gmail.com>
2019-12-13 09:40:11 +0000

    moved some ffiles to this repo

--- launch/grasping_in_sim/launchWithDelaysInBetween.sh
+++ launch/grasping_in_sim/launchWithDelaysInBetween.sh
@@ -3,9 +3,9 @@
 # dependencies: timed roslaunch
 # https://github.com/MoriKen254/timed_roslaunch
 
-rosrun timed_roslaunch timed_roslaunch.sh 0 niryo_one_bringup base_world_model.launch &
+rosrun timed_roslaunch timed_roslaunch.sh 0 icclab_grasping_niryo base_world_model.launch &
 
-rosrun timed_roslaunch timed_roslaunch.sh 10 niryo_one_bringup controllers.launch &
+rosrun timed_roslaunch timed_roslaunch.sh 10 icclab_grasping_niryo controllers.launch &
 
 rosrun timed_roslaunch timed_roslaunch.sh 20 niryo_one_bringup robot_commander.launch &
 
